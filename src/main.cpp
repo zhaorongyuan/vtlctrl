f4645a11160d52b3e48265f9869be7b637e0a87d
rongyuan <zhaorongyuan87@163.com>
2025-12-03 15:37:47 +0800

    main改为中文，取消demo界面

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,10 @@
-// Dear ImGui: standalone example application for Windows API + DirectX 11
+// Dear ImGui: Windows API + DirectX 11 独立示例应用程序
 
-// Learn about Dear ImGui:
-// - FAQ                  https://dearimgui.com/faq
-// - Getting Started      https://dearimgui.com/getting-started
-// - Documentation        https://dearimgui.com/docs (same as your local docs/ folder).
-// - Introduction, links and more at the top of imgui.cpp
+// 了解 Dear ImGui:
+// - 常见问题解答        https://dearimgui.com/faq
+// - 入门指南            https://dearimgui.com/getting-started
+// - 文档                https://dearimgui.com/docs (与本地 docs/ 文件夹相同).
+// - 简介、链接以及更多内容请见 imgui.cpp 顶部
 
 #include "imgui.h"
 #include "imgui_impl_win32.h"
@@ -12,34 +12,35 @@
 #include <d3d11.h>
 #include <tchar.h>
 
-// Data
-static ID3D11Device*            g_pd3dDevice = nullptr;
-static ID3D11DeviceContext*     g_pd3dDeviceContext = nullptr;
-static IDXGISwapChain*          g_pSwapChain = nullptr;
+// 数据
+static ID3D11Device* g_pd3dDevice = nullptr;
+static ID3D11DeviceContext* g_pd3dDeviceContext = nullptr;
+static IDXGISwapChain* g_pSwapChain = nullptr;
 static bool                     g_SwapChainOccluded = false;
 static UINT                     g_ResizeWidth = 0, g_ResizeHeight = 0;
-static ID3D11RenderTargetView*  g_mainRenderTargetView = nullptr;
+static ID3D11RenderTargetView* g_mainRenderTargetView = nullptr;
 
-// Forward declarations of helper functions
+// 辅助函数的前向声明
 bool CreateDeviceD3D(HWND hWnd);
 void CleanupDeviceD3D();
 void CreateRenderTarget();
 void CleanupRenderTarget();
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-// Main code
+// 主代码
 int main(int, char**)
 {
-    // Make process DPI aware and obtain main monitor scale
+    // 使进程感知 DPI 并获取主显示器的缩放比例
     ImGui_ImplWin32_EnableDpiAwareness();
+    //
     float main_scale = ImGui_ImplWin32_GetDpiScaleForMonitor(::MonitorFromPoint(POINT{ 0, 0 }, MONITOR_DEFAULTTOPRIMARY));
 
-    // Create application window
+    // 创建应用程序窗口
     WNDCLASSEXW wc = { sizeof(wc), CS_CLASSDC, WndProc, 0L, 0L, GetModuleHandle(nullptr), nullptr, nullptr, nullptr, nullptr, L"ImGui Example", nullptr };
     ::RegisterClassExW(&wc);
-    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Dear ImGui DirectX11 Example", WS_OVERLAPPEDWINDOW, 100, 100, (int)(1280 * main_scale), (int)(800 * main_scale), nullptr, nullptr, wc.hInstance, nullptr);
+    HWND hwnd = ::CreateWindowW(wc.lpszClassName, L"Dear ImGui DirectX11 示例", WS_OVERLAPPEDWINDOW, 100, 100, (int)(720 * main_scale), (int)(500 * main_scale), nullptr, nullptr, wc.hInstance, nullptr);
 
-    // Initialize Direct3D
+    // 初始化 Direct3D
     if (!CreateDeviceD3D(hwnd))
     {
         CleanupDeviceD3D();
@@ -47,37 +48,37 @@ int main(int, char**)
         return 1;
     }
 
-    // Show the window
+    // 显示窗口
     ::ShowWindow(hwnd, SW_SHOWDEFAULT);
     ::UpdateWindow(hwnd);
 
-    // Setup Dear ImGui context
+    // 设置 Dear ImGui 上下文
     IMGUI_CHECKVERSION();
     ImGui::CreateContext();
     ImGuiIO& io = ImGui::GetIO(); (void)io;
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
-    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // Enable Gamepad Controls
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // 启用键盘控制
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableGamepad;      // 启用游戏手柄控制
 
-    // Setup Dear ImGui style
+    // 设置 Dear ImGui 样式
     ImGui::StyleColorsDark();
     //ImGui::StyleColorsLight();
 
-    // Setup scaling
+    // 设置缩放
     ImGuiStyle& style = ImGui::GetStyle();
-    style.ScaleAllSizes(main_scale);        // Bake a fixed style scale. (until we have a solution for dynamic style scaling, changing this requires resetting Style + calling this again)
-    style.FontScaleDpi = main_scale;        // Set initial font scale. (using io.ConfigDpiScaleFonts=true makes this unnecessary. We leave both here for documentation purpose)
+    style.ScaleAllSizes(main_scale);        // 烘焙固定的样式缩放。(在我们要动态样式缩放的解决方案之前，更改此项需要重置样式 + 再次调用此函数)
+    style.FontScaleDpi = main_scale;        // 设置初始字体缩放。(使用 io.ConfigDpiScaleFonts=true 使这变得不必要。我们把两者都留在这里用于文档目的)
 
-    // Setup Platform/Renderer backends
+    // 设置平台/渲染器后端
     ImGui_ImplWin32_Init(hwnd);
     ImGui_ImplDX11_Init(g_pd3dDevice, g_pd3dDeviceContext);
 
-    // Load Fonts
-    // - If no fonts are loaded, dear imgui will use the default font. You can also load multiple fonts and use ImGui::PushFont()/PopFont() to select them.
-    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you need to select the font among multiple.
-    // - If the file cannot be loaded, the function will return a nullptr. Please handle those errors in your application (e.g. use an assertion, or display an error and quit).
-    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use Freetype for higher quality font rendering.
-    // - Read 'docs/FONTS.md' for more instructions and details. If you like the default font but want it to scale better, consider using the 'ProggyVector' from the same author!
-    // - Remember that in C/C++ if you want to include a backslash \ in a string literal you need to write a double backslash \\ !
+    // 加载字体
+    // - 如果没有加载字体，dear imgui 将使用默认字体。您也可以加载多个字体并使用 ImGui::PushFont()/PopFont() 来选择它们。
+    // - AddFontFromFileTTF() 将返回 ImFont*，因此如果您需要在多个字体中进行选择，可以存储它。
+    // - 如果文件无法加载，该函数将返回 nullptr。请在您的应用程序中处理这些错误（例如，使用断言，或显示错误并退出）。
+    // - 在您的 imconfig 文件中使用 '#define IMGUI_ENABLE_FREETYPE' 以使用 Freetype 进行更高质量的字体渲染。
+    // - 阅读 'docs/FONTS.md' 以获取更多说明和详细信息。如果您喜欢默认字体但希望它能更好地缩放，请考虑使用同一作者的 'ProggyVector'！
+    // - 请记住，在 C/C++ 中，如果您想在字符串字面量中包含反斜杠 \，您需要写入双反斜杠 \\ ！
     //style.FontSizeBase = 20.0f;
     //io.Fonts->AddFontDefault();
     //io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf");
@@ -87,17 +88,17 @@ int main(int, char**)
     //ImFont* font = io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf");
     //IM_ASSERT(font != nullptr);
 
-    // Our state
+    // 我们的状态
     bool show_demo_window = true;
     bool show_another_window = false;
     ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
-    // Main loop
+    // 主循环
     bool done = false;
     while (!done)
     {
-        // Poll and handle messages (inputs, window resize, etc.)
-        // See the WndProc() function below for our to dispatch events to the Win32 backend.
+        // 轮询并处理消息（输入、窗口大小调整等）
+        // 请参阅下面的 WndProc() 函数，了解我们要向 Win32 后端分派事件。
         MSG msg;
         while (::PeekMessage(&msg, nullptr, 0U, 0U, PM_REMOVE))
         {
@@ -109,7 +110,7 @@ int main(int, char**)
         if (done)
             break;
 
-        // Handle window being minimized or screen locked
+        // 处理窗口最小化或屏幕锁定
         if (g_SwapChainOccluded && g_pSwapChain->Present(0, DXGI_PRESENT_TEST) == DXGI_STATUS_OCCLUDED)
         {
             ::Sleep(10);
@@ -117,7 +118,7 @@ int main(int, char**)
         }
         g_SwapChainOccluded = false;
 
-        // Handle window resize (we don't resize directly in the WM_SIZE handler)
+        // 处理窗口大小调整（我们不直接在 WM_SIZE 处理程序中调整大小）
         if (g_ResizeWidth != 0 && g_ResizeHeight != 0)
         {
             CleanupRenderTarget();
@@ -126,62 +127,82 @@ int main(int, char**)
             CreateRenderTarget();
         }
 
-        // Start the Dear ImGui frame
+        // 开始 Dear ImGui 帧
         ImGui_ImplDX11_NewFrame();
         ImGui_ImplWin32_NewFrame();
         ImGui::NewFrame();
 
-        // 1. Show the big demo window (Most of the sample code is in ImGui::ShowDemoWindow()! You can browse its code to learn more about Dear ImGui!).
-        if (show_demo_window)
-            ImGui::ShowDemoWindow(&show_demo_window);
+        // // 1. 显示演示窗口（大多数示例代码都在 ImGui::ShowDemoWindow() 中！您可以浏览其代码以了解更多关于 Dear ImGui 的信息！）。
+        // if (show_demo_window)
+        //     ImGui::ShowDemoWindow(&show_demo_window);
+
+        // // 2. 显示我们自己创建的一个简单窗口。我们使用 Begin/End 对来创建一个命名窗口。
+        // {
+        //     static float f = 0.0f;
+        //     static int counter = 0;
+        //
+        //     ImGui::Begin("你好，世界！");                          // 创建一个名为 "你好，世界！" 的窗口并向其追加内容。
+        //
+        //     ImGui::Text("这是一些有用的文本。");                   // 显示一些文本（您也可以使用格式字符串）
+        //     ImGui::Checkbox("演示窗口", &show_demo_window);      // 编辑存储我们窗口打开/关闭状态的布尔值
+        //     ImGui::Checkbox("另一个窗口", &show_another_window);
+        //
+        //     ImGui::SliderFloat("浮点数", &f, 0.0f, 1.0f);            // 使用滑块编辑一个从 0.0f 到 1.0f 的浮点数
+        //     ImGui::ColorEdit3("清除颜色", (float*)&clear_color); // 编辑代表颜色的 3 个浮点数
+        //
+        //     if (ImGui::Button("按钮"))                            // 按钮被点击时返回 true（大多数小部件在编辑/激活时返回 true）
+        //         counter++;
+        //     ImGui::SameLine();
+        //     ImGui::Text("计数器 = %d", counter);
+        //
+        //     ImGui::Text("应用程序平均耗时 %.3f ms/帧 (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
+        //     ImGui::End();
+        // }
 
         // 2. Show a simple window that we create ourselves. We use a Begin/End pair to create a named window.
         {
             static float f = 0.0f;
             static int counter = 0;
-
             ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
-
             ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
             ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
             ImGui::Checkbox("Another Window", &show_another_window);
-
             ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
             ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
-
             if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
                 counter++;
             ImGui::SameLine();
             ImGui::Text("counter = %d", counter);
-
             ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io.Framerate, io.Framerate);
             ImGui::End();
         }
 
-        // 3. Show another simple window.
-        if (show_another_window)
-        {
-            ImGui::Begin("Another Window", &show_another_window);   // Pass a pointer to our bool variable (the window will have a closing button that will clear the bool when clicked)
-            ImGui::Text("Hello from another window!");
-            if (ImGui::Button("Close Me"))
-                show_another_window = false;
-            ImGui::End();
-        }
 
-        // Rendering
+
+        // // 3. 显示另一个简单窗口。
+        // if (show_another_window)
+        // {
+        //     ImGui::Begin("另一个窗口", &show_another_window);   // 传递指向我们要控制的布尔变量的指针（窗口将有一个关闭按钮，点击时会清除该布尔值）
+        //     ImGui::Text("来自另一个窗口的问候！");
+        //     if (ImGui::Button("关闭我"))
+        //         show_another_window = false;
+        //     ImGui::End();
+        // }
+
+        // 渲染
         ImGui::Render();
         const float clear_color_with_alpha[4] = { clear_color.x * clear_color.w, clear_color.y * clear_color.w, clear_color.z * clear_color.w, clear_color.w };
         g_pd3dDeviceContext->OMSetRenderTargets(1, &g_mainRenderTargetView, nullptr);
         g_pd3dDeviceContext->ClearRenderTargetView(g_mainRenderTargetView, clear_color_with_alpha);
         ImGui_ImplDX11_RenderDrawData(ImGui::GetDrawData());
 
-        // Present
-        HRESULT hr = g_pSwapChain->Present(1, 0);   // Present with vsync
-        //HRESULT hr = g_pSwapChain->Present(0, 0); // Present without vsync
+        // 呈现 (Present)
+        HRESULT hr = g_pSwapChain->Present(1, 0);   // 开启垂直同步呈现
+        //HRESULT hr = g_pSwapChain->Present(0, 0); // 关闭垂直同步呈现
         g_SwapChainOccluded = (hr == DXGI_STATUS_OCCLUDED);
     }
 
-    // Cleanup
+    // 清理
     ImGui_ImplDX11_Shutdown();
     ImGui_ImplWin32_Shutdown();
     ImGui::DestroyContext();
@@ -193,12 +214,12 @@ int main(int, char**)
     return 0;
 }
 
-// Helper functions
+// 辅助函数
 
 bool CreateDeviceD3D(HWND hWnd)
 {
-    // Setup swap chain
-    // This is a basic setup. Optimally could use e.g. DXGI_SWAP_EFFECT_FLIP_DISCARD and handle fullscreen mode differently. See #8979 for suggestions.
+    // 设置交换链
+    // 这是一个基本设置。最佳情况是使用例如 DXGI_SWAP_EFFECT_FLIP_DISCARD 并以不同方式处理全屏模式。请参阅 #8979 了解建议。
     DXGI_SWAP_CHAIN_DESC sd;
     ZeroMemory(&sd, sizeof(sd));
     sd.BufferCount = 2;
@@ -220,7 +241,7 @@ bool CreateDeviceD3D(HWND hWnd)
     D3D_FEATURE_LEVEL featureLevel;
     const D3D_FEATURE_LEVEL featureLevelArray[2] = { D3D_FEATURE_LEVEL_11_0, D3D_FEATURE_LEVEL_10_0, };
     HRESULT res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
-    if (res == DXGI_ERROR_UNSUPPORTED) // Try high-performance WARP software driver if hardware is not available.
+    if (res == DXGI_ERROR_UNSUPPORTED) // 如果硬件不可用，请尝试高性能 WARP 软件驱动程序。
         res = D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_WARP, nullptr, createDeviceFlags, featureLevelArray, 2, D3D11_SDK_VERSION, &sd, &g_pSwapChain, &g_pd3dDevice, &featureLevel, &g_pd3dDeviceContext);
     if (res != S_OK)
         return false;
@@ -250,14 +271,14 @@ void CleanupRenderTarget()
     if (g_mainRenderTargetView) { g_mainRenderTargetView->Release(); g_mainRenderTargetView = nullptr; }
 }
 
-// Forward declare message handler from imgui_impl_win32.cpp
+// 前向声明来自 imgui_impl_win32.cpp 的消息处理程序
 extern IMGUI_IMPL_API LRESULT ImGui_ImplWin32_WndProcHandler(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
-// Win32 message handler
-// You can read the io.WantCaptureMouse, io.WantCaptureKeyboard flags to tell if dear imgui wants to use your inputs.
-// - When io.WantCaptureMouse is true, do not dispatch mouse input data to your main application, or clear/overwrite your copy of the mouse data.
-// - When io.WantCaptureKeyboard is true, do not dispatch keyboard input data to your main application, or clear/overwrite your copy of the keyboard data.
-// Generally you may always pass all inputs to dear imgui, and hide them from your application based on those two flags.
+// Win32 消息处理程序
+// 您可以读取 io.WantCaptureMouse, io.WantCaptureKeyboard 标志来判断 dear imgui 是否想要使用您的输入。
+// - 当 io.WantCaptureMouse 为真时，不要将鼠标输入数据分发给您的主应用程序，或者清除/覆盖您的鼠标数据副本。
+// - 当 io.WantCaptureKeyboard 为真时，不要将键盘输入数据分发给您的主应用程序，或者清除/覆盖您的键盘数据副本。
+// 通常，您总是可以将所有输入传递给 dear imgui，并根据这两个标志对您的应用程序隐藏它们。
 LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
 {
     if (ImGui_ImplWin32_WndProcHandler(hWnd, msg, wParam, lParam))
@@ -268,11 +289,11 @@ LRESULT WINAPI WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam)
     case WM_SIZE:
         if (wParam == SIZE_MINIMIZED)
             return 0;
-        g_ResizeWidth = (UINT)LOWORD(lParam); // Queue resize
+        g_ResizeWidth = (UINT)LOWORD(lParam); // 队列调整大小
         g_ResizeHeight = (UINT)HIWORD(lParam);
         return 0;
     case WM_SYSCOMMAND:
-        if ((wParam & 0xfff0) == SC_KEYMENU) // Disable ALT application menu
+        if ((wParam & 0xfff0) == SC_KEYMENU) // 禁用 ALT 应用程序菜单
             return 0;
         break;
     case WM_DESTROY:
